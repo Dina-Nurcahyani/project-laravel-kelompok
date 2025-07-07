73334396cbc871e669cc2c9889706913abccd132
Noviya06 <nopianaicha29@gmail.com>
2025-07-07 17:27:12 +0700

    tambah fitur dashboard

--- resources/views/components/brand.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="flex items-center space-x-2">
-    <img src="{{ asset('logo.png') }}" class="h-8 w-8" alt="Logo">
-    <span class="text-lg font-bold text-white">Informasi Pasien RSJ</span>
-</div>
